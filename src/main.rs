66448cb37a3a390764ebbf276c50d4590b955580
G̅̈́͏̣̖ͅl̞͕̊̽ͫ̍͟i̯̝̮̥̺̤͛̃̍́t̢͖̳͈̱̦̺̤̅c͔̼̰͓͈̣̊ͬ̔̒͢h̨͖̙̟̗͇̣̦͌ <unmemorableusername0@gmail.com>
2023-09-28 21:46:42 -0400

    Request Test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use yew::prelude::*;
+use gloo_net::http::Request;
 
 #[function_component]
 fn App() -> Html {
+    let get_request = Request::get("https://hcpss.instructure.com/api/v1/courses/228282/assignments");
     html! {
         <div>
-            <span> { "Hello World!" } </span>
+            <span> { format!("{:?}", get_request) } </span>
         </div>
     }
 }
